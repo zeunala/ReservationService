c38e9217369762b1889d56ee41ff79298c048bbf
zeunala <zeunala@naver.com>
2022-05-02 14:37:37 +0900

    feat: 예약 한줄평 관련 Controller 내용 추가

--- src/main/java/zeunala/reservation/controller/ReservationApiController.java
+++ src/main/java/zeunala/reservation/controller/ReservationApiController.java
@@ -1,18 +1,18 @@
 package zeunala.reservation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import zeunala.reservation.dto.CommentResult;
 import zeunala.reservation.dto.ReservationByEmail;
 import zeunala.reservation.dto.ReservationParam;
 import zeunala.reservation.dto.ReservationResult;
 import zeunala.reservation.service.ReservationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping(path="/api")
@@ -39,5 +39,47 @@ public class ReservationApiController {
 	public ReservationResult cancelReservation(@PathVariable Integer reservationId) {
 		return reservationService.cancelReservation(reservationId);
 	}
-	
+
+	@PostMapping(path="/reservations/{reservationInfoId}/comments", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public CommentResult addComment(@PathVariable Integer reservationInfoId,
+									@RequestParam("comment") String comment,
+									@RequestParam("productId") Integer productId,
+									@RequestParam("score") Double score,
+									@RequestParam(required=false) MultipartFile attachedImage,
+									HttpServletRequest request) throws IOException {
+		final String IMAGE_COMMENT_PATH = request.getServletContext().getRealPath(("/img_comment/"));
+		String imageFileName = null;
+
+		// score, comment 길이 유효성 검사
+		if (score < 0 || score > 5 || comment.length() < 5 || comment.length() > 400) {
+			return null;
+		}
+
+		// 첨부된 파일이 있을 경우 업로드
+		if (attachedImage != null) {
+			imageFileName = attachedImage.getOriginalFilename();
+
+			// 중복 파일명 처리
+			File targetFile = new File(IMAGE_COMMENT_PATH + imageFileName);
+			int fileNumber = 1;
+			while (targetFile.exists()) {
+				String originalImageFileName = attachedImage.getOriginalFilename();
+				fileNumber++;
+				String newImageFileName = originalImageFileName.substring(0, originalImageFileName.lastIndexOf("."))
+						+ " (" + Integer.toString(fileNumber) + ")"
+						+ originalImageFileName.substring(originalImageFileName.lastIndexOf("."));
+				targetFile = new File(IMAGE_COMMENT_PATH + newImageFileName);
+				imageFileName = newImageFileName;
+			}
+
+			// 확장자 유효성 검사
+			String fileNameExtension = imageFileName.substring(imageFileName.lastIndexOf(".") + 1);
+			if (!fileNameExtension.equals("jpg") && !fileNameExtension.equals("jpeg") && !fileNameExtension.equals("png")) {
+				return null;
+			}
+
+			attachedImage.transferTo(new File(IMAGE_COMMENT_PATH + imageFileName));
+		}
+		return reservationService.addComment(reservationInfoId, comment, productId, score, imageFileName);
+	}
 }
--- src/main/java/zeunala/reservation/controller/ReservationController.java
+++ src/main/java/zeunala/reservation/controller/ReservationController.java
@@ -2,12 +2,15 @@ package zeunala.reservation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import zeunala.reservation.dto.Reservation;
 import zeunala.reservation.service.ReservationService;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class ReservationController {
@@ -65,5 +68,25 @@ public class ReservationController {
 	public String reserve() {
 
 		return "reserve";
+	}
+
+	@PostMapping(path="/review_write")
+	public String review_write(@RequestParam String displayid, @RequestParam String reserveid, HttpSession session, ModelMap modelMap) {
+		if (session.getAttribute("loginEmail") != null) {
+			List<Reservation> reservationList = reservationService.getReservations((String)session.getAttribute("loginEmail"));
+
+			// reservationList돌면서 displayid와 reserveid 모두 일치하는게 있으면 reviewWrite보여주고 아니면 비정상접근으로 간주해 mainpage로 돌려보냄
+			for (Reservation i : reservationList) {
+				if (i.getDisplayInfoId().equals(Integer.parseInt(displayid))
+						&& i.getReservationInfoId().equals(Integer.parseInt(reserveid))) {
+					modelMap.addAttribute("displayId", Integer.parseInt(displayid));
+					modelMap.addAttribute("reserveId", Integer.parseInt(reserveid));
+					return "reviewWrite";
+				}
+			}
+			return "mainpage";
+		} else {
+			return "bookinglogin"; // 로그인 되어있지 않은 상태에서 url로 접근시 로그인 페이지로 이동
+		}
 	}
 }
